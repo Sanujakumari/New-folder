87104df21f05b62adf2779c48d25bace0ee64493
Sanuja kumari <sanujakumari@gmail.com>
2021-11-25 09:02:57 +0530

    routes ("/")

--- routes/movie.js
+++ routes/movie.js
@@ -1,6 +1,18 @@
 import express from "express";
+import{
+    getMovieByName,
+    deleteMovieById,
+    deleteByQuery,
+    getMovieById,
+    createMovie,
+    getMovieByQuery,
+    updateMovieByName
+  } from  "../helper.js"
 const router=express.Router();
-router.get("/movies", async(request, response) => {
+
+router
+.route('/')
+.get(async(request, response) => {
     console.log(request.query);
     let filter=request.query;
    
@@ -16,15 +28,8 @@ router.get("/movies", async(request, response) => {
     //Query is for filtering
    //  const movlang=movies.filter((mv)=>mv.language===language)
     // response.send( movlang ? movlang : {});
-   });
-   router.get("/movies/:id", async(request, response) => {
-     const {id}=request.params;
-    const movie = await getMovieById(id);
- 
- response.send( movie ? movie : {message:"No matching"})
-   
- });
- router.delete("/movies", async(request, response) => {
+   })
+    .delete( async(request, response) => {
    console.log(request.query);
    let filter=request.query;
   
@@ -35,30 +40,41 @@ router.get("/movies", async(request, response) => {
  
    const deletequery = await deleteByQuery(filter);
    response.send(deletequery)
-  });
-  router.delete("/movies/:id", async(request, response) => {
+  })
+  .post( async(request, response) => {
+    const data=request.body;
+      const result = await createMovie(data);
+    
+    response.send(result )
+    
+    })
+    .put(async(request, response) => {
+        const {name}=request.query;
+        console.log(request.query, request.body);
+        const client = await updateMovieByName(name, request);
+      
+        const movie = await getMovieByName(name);
+      response.send(movie);
+      
+      });
+ 
+      router
+      .route("/:id")
+      .delete( async(request, response) => {
    const {id}=request.params;
   const delmovie = await deleteMovieById(id);
  
  response.send( delmovie ? delmovie : {message:"No matching"})
  
- });
- router.put("/movies", async(request, response) => {
-   const {name}=request.query;
-   console.log(request.query, request.body);
-   const client = await updateMovieByName(name, request);
+ })
+ .get( async(request, response) => {
+    const {id}=request.params;
+   const movie = await getMovieById(id);
+
+response.send( movie ? movie : {message:"No matching"})
+  
+})
  
-   const movie = await getMovieByName(name);
- response.send(movie);
  
- });
- 
- router.post("/movies", async(request, response) => {
- const data=request.body;
-   const result = await createMovie(data);
- 
- response.send(result )
- 
- });
  
  export const movieRouter = router;
